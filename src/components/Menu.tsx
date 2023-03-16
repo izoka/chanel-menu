b969938b175aa871e2e86ae210ad40f349788995
Maxime <maximechampeval@gmail.com>
2023-03-16 21:11:58 +0100

    Fix/clean componants

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,74 +1,12 @@
 import React, {useEffect, useMemo, useState} from "react";
-import { AiFillCaretRight } from "react-icons/ai";
-import { AiFillCaretLeft } from "react-icons/ai";
+import { FormattedMenuData, formattedMenuData } from "../utils/menu";
+import MenuOptions from "./MenuOptions";
 
 import "./Menu.css";
-import { FormattedMenuData, formattedMenuData } from "../utils/menu";
 
 interface MenuProps {
     data: FormattedMenuData[];
 }
-
-interface MenuOptionsProps {
-    formattedOptions: FormattedMenuData[];
-    onClickSubMenu: (option: FormattedMenuData) => void;
-    onCLickPrevMenu: (parentId: number | null) => void;
-    position: number;
-    currentScreenPosition: number;
-    selectedOption: FormattedMenuData | null;
-}
-
-
-const MenuOptions = ({ formattedOptions, onClickSubMenu, onCLickPrevMenu, position, currentScreenPosition, selectedOption }: MenuOptionsProps): JSX.Element => {
-    const localPosition = 0;
-    return (
-        <ul className={`${!formattedOptions[0].parent && 'transition'}`} style={{left: `${!formattedOptions[0].parent ? currentScreenPosition : position}%`, width: "100%"}}>
-            {formattedOptions.map((option, index) => (
-               <span key={index}>
-                   {index === 0 && (
-                       <>
-                           {option && option.parent && (
-                               <li onClick={(e) => {
-                                   e.stopPropagation();
-                                   onCLickPrevMenu(option.parent)
-                               }}>
-                                   <div className="menu-button">
-                                       <AiFillCaretLeft />
-                                   </div>
-                               </li>
-                           )}
-                       </>
-                   )}
-                   <li style={{width: "100%"}} key={option.id}>
-                       <div className={`menu-button ${(selectedOption?.id === option.parent || !selectedOption) ? 'flex' : 'none'}`} onClick={(e) => {
-                           if (!option.children && option.url) {
-                               window.open(option.url, "_blank")
-                           } else {
-                               e.stopPropagation();
-                               onClickSubMenu(option);
-                           }
-                       }}>
-                           {option.title}
-                           {option.children && <AiFillCaretRight />}
-                       </div>
-
-                       {option.children &&
-                           <MenuOptions
-                               position={localPosition + 100}
-                               onClickSubMenu={onClickSubMenu}
-                               onCLickPrevMenu={onCLickPrevMenu}
-                               formattedOptions={option.children}
-                               currentScreenPosition={currentScreenPosition}
-                               selectedOption={selectedOption}
-                           />
-                       }
-                   </li>
-               </span>
-            ))}
-        </ul>
-    )
-}
-
 
 const Menu: React.FC<MenuProps> = ({ data }) => {
     const [currentScreenPosition, setCurrenScreenPosition] = useState<number>(0);
